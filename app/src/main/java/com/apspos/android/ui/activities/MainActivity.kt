4ad490e72efd78c8af04342a7a5b66288fea3825
mauricioBit <mguzman@apsposus.com>
2021-12-10 16:53:59 -0500

    new version example.

--- app/src/main/java/com/apspos/android/ui/activities/MainActivity.kt
+++ app/src/main/java/com/apspos/android/ui/activities/MainActivity.kt
@@ -3,14 +3,23 @@ package com.apspos.android.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.apspos.android.R
+import com.apspos.android.databinding.ActivityMainBinding
 import com.github.javiersantos.appupdater.AppUpdater
 import com.github.javiersantos.appupdater.enums.Display
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            binding.textView.text = "Hello Mauricio !!!"
+        }
 
         val appUpdater = AppUpdater(this)
         appUpdater.setDisplay(Display.DIALOG)
