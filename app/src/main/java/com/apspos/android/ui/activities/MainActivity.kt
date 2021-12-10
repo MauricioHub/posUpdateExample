a545b45f154b55495e59561ff250acc5d6864bc3
mauricioBit <mguzman@apsposus.com>
2021-12-10 17:04:47 -0500

    example for Updating.

--- app/src/main/java/com/apspos/android/ui/activities/MainActivity.kt
+++ app/src/main/java/com/apspos/android/ui/activities/MainActivity.kt
@@ -6,6 +6,7 @@ import com.apspos.android.R
 import com.apspos.android.databinding.ActivityMainBinding
 import com.github.javiersantos.appupdater.AppUpdater
 import com.github.javiersantos.appupdater.enums.Display
+import com.github.javiersantos.appupdater.enums.UpdateFrom
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,11 +19,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.button.setOnClickListener {
-            binding.textView.text = "Hello Mauricio !!!"
+            binding.textView2.text = "This is an Example!"
+            binding.textView.text = "of App Updated!"
         }
 
         val appUpdater = AppUpdater(this)
         appUpdater.setDisplay(Display.DIALOG)
+        appUpdater.setUpdateFrom(UpdateFrom.GITHUB)
+        appUpdater.setGitHubUserAndRepo("MauricioHub", "posUpdateExample")
         appUpdater.start()
     }
 }
